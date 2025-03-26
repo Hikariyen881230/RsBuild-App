dfdd7eafb00cfa8269804f413b9639706f3effab
keith_liu <hikariyen881230@gmail.com>
2025-03-26 10:15:47 +0800

    調整

--- src/components/scanner.tsx
+++ src/components/scanner.tsx
@@ -74,7 +74,7 @@ export const Scanner = ({
 
           // 啟動新相機
           await html5Qrcode.current.start(
-            { facingMode: 'environment' },
+            { facingMode: { exact: 'environment' } },
             config,
             onCodeScanned,
             () => {},
